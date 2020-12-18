bf455b7d4e5e6c7565c958034a9cfbed89691f00
haarikkaota <haarikakota@gmail.com>
2020-12-18 12:32:47 -0600

    📦 NEW: Picture-in-picture using Screen Capture API

---
+++ picture-in-picture/index.js
@@ -0,0 +1,22 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectVideoStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error){
+        console.log('Whoops, Error occured:', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+});
+
+selectVideoStream();
